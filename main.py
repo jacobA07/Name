6db963c2a21e2e7597ee702da08e1e6bfa9a7aeb
jacobalden25 <122410371+jacobA07@users.noreply.github.com>
2023-01-11 23:29:39 +0000

    nothin

---
+++ main.py
@@ -0,0 +1,8 @@
+
+askname = input(" what is your name?")
+
+print("nice to meet you " + askname)
+
+sport = input("whats your favorite sport?")
+print("I like " + sport + " too!")
+print("good talk, have a nice day!")
